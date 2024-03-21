74c2f3985267ae1fd969cf59602b778f847f6235
kriostt <129346100+kriostt@users.noreply.github.com>
2024-03-20 18:37:43 -0600

    Create initial project in IntelliJ

---
+++ A3SolidPrinciples/src/main/java/CPRO2221/A3SolidPrinciples/Main.java
@@ -0,0 +1,7 @@
+package CPRO2221.A3SolidPrinciples;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("CPRO 2221 Assignment 3: Solid Principles");
+    }
+}
